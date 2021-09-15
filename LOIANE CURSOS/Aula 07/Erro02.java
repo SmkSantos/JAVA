24cc23e46e710d93caccc2732d0bd0ef6a44b9e2
Samuel <samuca.asap@gmail.com>
2021-09-15 00:06:46 -0300

    * Java Aula introdutória sobre erros e alguns processos de funcionamento do Java

---
+++ LOIANE CURSOS/Aula 07/Erro02.java
@@ -0,0 +1,15 @@
+class Erro02 {
+	
+	public static void main (String[] args){
+		
+		//var int cont;
+		//esse var não está na semantica quando declarado dessa forma
+
+		 //não foi atribuido nenhum valor as variáveis
+		int cont;
+		System.out.println("Hello World" + cont);
+
+
+		
+	}
+}
---
+++ LOIANE CURSOS/Aula 07/Erro03.java
@@ -0,0 +1,8 @@
+class Erro03 {
+	// O Main maiusc. vira Classe e o minusculo é metodo inicial
+	public static void Main (String[] args){
+		System.out.Println("Você digitou: ");
+	// o java é case sensitive, ou seja, o println tem que ser minusculo
+
+	}
+}
